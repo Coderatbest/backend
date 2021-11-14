62b7961606b6811e2ce6a066ed93bc3a275b9507
al3xdiaz <alexleonel96@hotmail.com>
2021-11-13 23:25:00 -0600

    add maria db

--- Readme.md
+++ Readme.md
@@ -18,3 +18,14 @@ python3  manage.py startapp utilies
 
 - edit file settings.py
 - add varible AUTH_USER_MODEL='users.User'
+
+## add maria db
+### add to requirements.txt
+- dj-database-url==0.5.0
+- environ==1.0
+- mysqlclient 2.0.3
+
+### envs 
+more information https://github.com/jacobian/dj-database-url
+- db_pass=password
+- DB_URL=mysql://USER:PASSWORD@HOST:PORT/NAME
--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -9,8 +9,9 @@ https://docs.djangoproject.com/en/3.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
-
+import environ
 from pathlib import Path
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -75,11 +76,8 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+DATABASES={
+    'default': dj_database_url.config(conn_max_age=600)
 }
 
 
