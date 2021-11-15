7dd0d729efe269a9cac6fbaa71e8c8605f5a12c6
al3xdiaz <alexleonel96@hotmail.com>
2021-11-14 18:13:08 -0600

    feat urls

---
+++ backend/posts/urls.py
@@ -0,0 +1,15 @@
+# #django
+# from django.urls import path,include
+# # rest frameworks
+# from rest_framework.routers import DefaultRouter
+# # models
+# from .views import users as user_views
+
+
+# router = DefaultRouter()
+
+# router.register(r'users',user_views.UsersViewSet,basename='auth')
+
+# urlpatterns = [
+#     path('',include(router.urls))
+# ]
