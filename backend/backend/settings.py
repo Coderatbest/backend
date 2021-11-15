6a65eefff053292148b5454c56808a7d2cc4839a
al3xdiaz <alexleonel96@hotmail.com>
2021-11-14 18:22:47 -0600

    add apps to apps_install

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -43,7 +43,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'users'
+    'users',
+    'commentaries',
+    'posts',
+    'visits'
 ]
 
 MIDDLEWARE = [
