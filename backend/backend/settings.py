2089afdbe71178c526149ca5268e9ccdadba5676
al3xdiaz <alexleonel96@hotmail.com>
2021-11-17 20:36:30 -0600

    change DEBUG

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-dc4t95$q3v99glye(%nh*c+a9fw_iqgucjhk$1e46cr2g4avjj
 ADMIN_URL=env('ADMIN_URL')
 # SECURITY WARNING: don't run with debug turned on in production!
 
-DEBUG = env.bool('DEPLOYED', default=True)
+DEBUG = env.bool('DEBUG ', default=True)
 
 ALLOWED_HOSTS = [] if DEBUG else str(env('ALLOWED_HOSTS')).split(',')
 
