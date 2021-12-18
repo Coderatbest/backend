a6d2dee578328a72438a8f5d5dec233aa6c7f596
al3xdiaz <alexleonel96@hotmail.com>
2021-12-17 22:15:25 -0600

    feat: send email

--- backend/backend/deploy.py
+++ backend/backend/deploy.py
@@ -5,17 +5,16 @@ ALLOWED_HOSTS = str(env('ALLOWED_HOSTS')).split(',')
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY=env('SECRET_KEY')
 
+WSGI_APPLICATION = 'backend.asgi.application'
 # anymail
 INSTALLED_APPS = +[
     # ...
-    "anymail",
     # ...
 ]
-ANYMAIL = {
-    # (exact settings here depend on your ESP...)
-    "MAILGUN_API_KEY": "<your Mailgun key>",
-    "MAILGUN_SENDER_DOMAIN": env('MAILGUN_SENDER_DOMAIN'),  # your Mailgun domain, if needed
-}
-EMAIL_BACKEND = "anymail.backends.mailgun.EmailBackend"  # or sendgrid.EmailBackend, or...
-DEFAULT_FROM_EMAIL =env('DEFAULT_FROM_EMAIL') #"you@example.com"  if you don't already have this in settings
-SERVER_EMAIL =env('SERVER_EMAIL')  #"your-server@example.com" ditto (default from-email for Django errors)
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_USE_TLS = True
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST_USER = env('MAIL_USER')
+EMAIL_HOST_PASSWORD = env('MAIL_PASS')
+EMAIL_PORT = 587
--- backend/backend/local.py
+++ backend/backend/local.py
@@ -4,6 +4,8 @@ print(f"http://localhost:8000/{ADMIN_URL}")
 
 ALLOWED_HOSTS = []
 
+
+WSGI_APPLICATION = 'backend.wsgi.application'
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'django-insecure-dc4t95$q3v99glye(%nh*c+a9fw_iqgucjhk$1e46cr2g4avjj'
 
