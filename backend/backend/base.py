1336e17862c26c61846e7f32a40534096516a398
al3xdiaz <alexleonel96@hotmail.com>
2021-12-23 20:05:14 -0600

    fix volumes

--- backend/backend/base.py
+++ backend/backend/base.py
@@ -118,7 +118,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = '/staticfiles/'
 STATIC_ROOT= 'mediafiles'
 
 # Default primary key field type
