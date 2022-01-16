de94630adef8b7542b4b921715b0d2c9b3d089aa
al3xdiaz <alexleonel96@hotmail.com>
2022-01-15 20:29:37 -0600

    fix auth try 4

--- backend/posts/views.py
+++ backend/posts/views.py
@@ -1,7 +1,7 @@
 # django rest frameworks
 from datetime import datetime
 from rest_framework import serializers, viewsets
-from rest_framework.permissions import IsAuthenticated
+from rest_framework.permissions import IsAuthenticated,AllowAny
 from rest_framework.response import Response
 from rest_framework import status
 from rest_framework.parsers import MultiPartParser, FormParser, JSONParser
@@ -17,5 +17,11 @@ class PostsViewSets(viewsets.ModelViewSet):
     parser_classes = (MultiPartParser,FormParser,JSONParser)
     queryset = PostsModels.objects.filter(is_activate=True)
     serializer_class = PostsModelsSerializers
-    permission_classes = (IsAuthenticated,)
     lookup_field = 'id'
+
+    def get_permissions(self):
+        """Assign permissions based on action."""
+        permissions = [AllowAny]
+        if self.action in ['create','update', 'partial_update','destroy']:
+            permissions.append(IsAuthenticated)
+        return [permission() for permission in permissions]
